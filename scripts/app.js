f5b11fd7f190e1a549ce5e0c9010760e48c0be8a
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-26 14:25:00 +0200

    add book element constructor

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,59 @@
 let library = [];
 
 function Book (title, author) {
-    this.title = title;
-    this.author = author;
+
+  this.title = title;
+  this.author = author;
+
 }
 
 function addBookToLibrary (book) {
-    library.push(book);
+
+  library.push(book);
+
 }
+
+const addBooks = function () {
+
+  library.forEach((book) => {
+
+    createBookElement(book);
+
+  });
+
+};
+
+const createBookElement = function (book) {
+
+  const container = document.getElementsByClassName('container');
+
+  // create book div
+  const div = document.createElement('div');
+  div.className = 'book';
+
+  // create title paragraph
+  const titleElement = document.createElement('p');
+  titleElement.className = 'title';
+  titleElement.innerText = book.title;
+
+  // create author paragraph
+  const authorElement = document.createElement('p');
+  authorElement.className = 'author';
+  authorElement.innerText = book.author;
+
+  div.appendChild(titleElement);
+  div.appendChild(authorElement);
+
+  container[0].appendChild(div);
+
+};
+
+for (let i = 0; i < 3; i++) {
+
+  let newbook = new Book(prompt("Enter title"), prompt("Enter author"));
+  library.push(newbook);
+
+}
+
+addBooks();
+
