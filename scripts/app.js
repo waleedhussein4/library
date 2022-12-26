7d3cecf5eeabc068154e9444fb5a9c9c427265db
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-26 16:26:02 +0200

    update app.js
    - add listeners
    - add constructors
    - add add book button
    - add remove button
    - rewrite book addition process

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,49 @@
+// GLOBAL VARIABLES
+index = 0;
 let library = [];
+// GLOBAL VARIABLES
 
+
+
+// LISTENERS
+document.getElementById('add').addEventListener('click', function () {
+  generateBook();
+});
+
+document.getElementsByClassName('remove').array.forEach(button => {
+  button.addEventListener('click', function () {
+    removeBook(button.id);
+  });
+});
+// LISTENERS
+
+
+
+// OBJECTS
 function Book (title, author) {
 
   this.title = title;
   this.author = author;
+  this.id = index++;
 
 }
+// OBJECTS
+
+
+
+// FUNCTIONS
+const generateBook = function () {
+  let title = prompt("Enter TITLE");
+  let author = prompt("Enter AUTHOR");
+  let book = new Book(title, author);
+  addBookToLibrary(book);
+};
 
 function addBookToLibrary (book) {
 
-  library.push(book);
-
-}
-
-const addBooks = function () {
-
-  library.forEach((book) => {
-
-    createBookElement(book);
-
-  });
+  library[book.id] = book;
+  createBookElement(book);
+  console.log(library);
 
 };
 
@@ -30,6 +54,7 @@ const createBookElement = function (book) {
   // create book div
   const div = document.createElement('div');
   div.className = 'book';
+  div.dataset.id = book.id;
 
   // create title paragraph
   const titleElement = document.createElement('p');
@@ -39,21 +64,32 @@ const createBookElement = function (book) {
   // create author paragraph
   const authorElement = document.createElement('p');
   authorElement.className = 'author';
-  authorElement.innerText = book.author;
+  authorElement.innerHTML = 'by ' + `<strong> ${book.author}`;
+
+  // create id paragraph
+  const idElement = document.createElement('p');
+  idElement.className = 'id';
+  idElement.innerText = book.id;
+
+  // create remove button
+  const buttonElement = document.createElement('button');
+  buttonElement.id = book.id;
+  buttonElement.classList.add('remove');
+  buttonElement.innerText = 'Remove';
 
   div.appendChild(titleElement);
   div.appendChild(authorElement);
+  div.appendChild(buttonElement);
+  div.dataset.id = book.id;
 
   container[0].appendChild(div);
 
 };
 
-for (let i = 0; i < 3; i++) {
+const removeBook = function (id) {
+  // remove the book from the library array
+  // remove the book from the page
+};
+// FUNCTIONS
 
-  let newbook = new Book(prompt("Enter title"), prompt("Enter author"));
-  library.push(newbook);
-
-}
-
-addBooks();
-
+console.log(library);
