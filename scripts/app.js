fa758e3fd8761874ff368b45d1c5a6077ec7065f
Waleed Hussein <62118101+Waleed405@users.noreply.github.com>
2022-12-26 21:47:45 +0200

    rewrite createBookElement() as Book.prototype.createElement()

--- scripts/app.js
+++ scripts/app.js
@@ -37,54 +37,46 @@ function Book (title, author) {
 
 Book.prototype.store = function () {
   library[this.id] = this;
-  createBookElement(this);
+  // createBookElement(this);
+  this.createElement();
   console.log(library);
 };
 
-
-const generateBook = function () {
-  let title = prompt("Enter TITLE");
-  let author = prompt("Enter AUTHOR");
-  let book = new Book(title, author);
-
-  book.store();
-};
-
-const createBookElement = function (book) {
+Book.prototype.createElement = function () {
 
   const container = document.getElementsByClassName('container');
 
   // create book div
   const div = document.createElement('div');
   div.className = 'book';
-  div.id = 'book-' + book.id;
+  div.id = 'book-' + this.id;
   div.dataset.read = 'false';
 
   // create title paragraph
   const titleElement = document.createElement('p');
   titleElement.className = 'title';
-  titleElement.innerText = book.title;
+  titleElement.innerText = this.title;
 
   // create author paragraph
   const authorElement = document.createElement('p');
   authorElement.className = 'author';
-  authorElement.innerHTML = 'by ' + `<strong> ${book.author}`;
+  authorElement.innerHTML = 'by ' + `<strong> ${this.author}`;
 
   // create id paragraph
   const idElement = document.createElement('p');
   idElement.className = 'id';
-  idElement.innerText = 'ID: ' + book.id;
+  idElement.innerText = 'ID: ' + this.id;
 
   // create remove button
   const buttonElement = document.createElement('button');
-  buttonElement.id = book.id;
+  buttonElement.id = this.id;
   buttonElement.classList.add('remove');
   buttonElement.innerText = 'Remove';
   addRemoveListener(buttonElement);
 
   // create read paragraph
   const readElement = document.createElement('button');
-  readElement.id = book.id;
+  readElement.id = this.id;
   readElement.classList.add('read');
   readElement.innerText = 'NOT READ';
   readElement.style.backgroundColor = '#ff9c9c';
@@ -98,6 +90,14 @@ const createBookElement = function (book) {
 
   container[0].appendChild(div);
 
+};
+
+const generateBook = function () {
+  let title = prompt("Enter TITLE");
+  let author = prompt("Enter AUTHOR");
+  let book = new Book(title, author);
+
+  book.store();
 };
 
 const removeBook = function (id) {
